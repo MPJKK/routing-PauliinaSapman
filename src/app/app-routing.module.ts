92009f5fc90b332c266ef4aa6867ab309f6490d8
Pauliina Sapman <pauliips@ety-a2123b-29.edu.wks.metropolia.fi>
2018-01-30 11:09:35 +0200

    v3t1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RoutesComponent} from './routes/routes.component';
+import { SetupComponent} from './setup/setup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path: 'routes', component: RoutesComponent
+    }, {
+      path: '', component: SetupComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, } from '@angular/core';
+
+import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { SetupComponent } from './setup/setup.component';
+import { RoutesComponent } from './routes/routes.component';
+import {HttpClientModule} from '@angular/common/http';
+import {DigitransitService} from './services/digitransit.service';
+import {FormsModule} from '@angular/forms';
+import { RouteDirectionPipe } from './pipes/route-direction.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    SetupComponent,
+    RoutesComponent,
+    RouteDirectionPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+      HttpClientModule,
+      FormsModule,
+      AgmCoreModule.forRoot({
+          apiKey: 'AIzaSyAxVeP4eBhU2lZmGN_6Oxizxoahl1W7pcY '
+      })
   ],
-  providers: [],
+  providers: [DigitransitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
